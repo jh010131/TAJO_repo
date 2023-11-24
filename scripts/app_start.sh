12e9d5784be0f2b4e71ea73a436b0bdf819f7a71
jh010131 <loojinhwan@gmail.com>
2023-11-24 05:35:16 +0000

    modify app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,25 +3,11 @@
 source /etc/profile
 cd /home/ec2-user
 
-BUILD_WAR="/home/ec2-user/build/*.war"
-# echo "> build 파일명: $WAR_NAME" >> /home/ec2-user/deploy.log
+BUILD_WAR="/home/ec2-user/build/spring-petclinic.war"
+DEPLOY_PATH="$CATALINA_HOME/webapps/"
 
-# echo "> build 파일 복사" >> /home/ec2-user/deploy.log
-DEPLOY_PATH="/home/tomcat/apache-tomcat-10.1.12/webapps/"
 sudo chmod +x $DEPLOY_PATH
 sudo cp $BUILD_WAR $DEPLOY_PATH
 
-# echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/deploy.log
-# CURRENT_PID=$(pgrep -f $WAR_NAME)
-
-# if [ -z $CURRENT_PID ]
-# then
-#   echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/deploy.log
-# else
-#   echo "> kill -15 $CURRENT_PID"
-#   kill -15 $CURRENT_PID
-#   sleep 5
-# fi
-
-# echo "> DEPLOY_WAR 배포"    >> /home/ec2-user/deploy.log
-sudo service tomcat restart
+sudo service tomcat stop
+sudo service tomcat start
