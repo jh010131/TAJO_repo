eb9a6e452959a82b8bc7614bc5cb0cdd913d52e2
jh010131 <loojinhwan@gmail.com>
2023-11-27 05:28:52 +0000

    modify app deploy

--- src/scripts/app_start.sh
+++ src/scripts/app_start.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-source /etc/profile
-cd /home/ec2-user
-
 BUILD_WAR="/home/ec2-user/build/spring-petclinic.war"
-DEPLOY_PATH="$CATALINA_HOME/webapps/"
+DEPLOY_PATH="/home/tomcat/apache-tomcat-10.1.12/webapps"
+
 
 sudo chmod +x $DEPLOY_PATH
+
+sudo rm -rf $DEPLOY_PATH/*
+
 sudo cp $BUILD_WAR $DEPLOY_PATH
 
-sudo service tomcat stop
-sudo service tomcat start
+
+sudo /home/tomcat/apache-tomcat-10.1.12/bin/shutdown.sh
+sudo /home/tomcat/apache-tomcat-10.1.12/bin/startup.sh
