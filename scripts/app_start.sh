8d7cbce9c2c9ad068143cc16035603fcb1291c37
jh010131 <loojinhwan@gmail.com>
2023-11-29 07:40:00 +0000

    modify app_start.sh

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-BUILD_WAR="/home/ec2-user/build/spring-petclinic.war"
-DEPLOY_PATH="/home/tomcat/apache-tomcat-10.1.12/webapps"
+BUILD_WAR="/home/ec2-user/build/spring-petclinic.war" 
+DEPLOY_PATH="/home/tomcat/apache-tomcat-10.1.12/webapps/"
 TOMCAT_STOP="/home/tomcat/apache-tomcat-10.1.12/bin/shutdown.sh"
 TOMCAT_START="/home/tomcat/apache-tomcat-10.1.12/bin/startup.sh"
 
 sudo chmod +x $DEPLOY_PATH
+
+sudo rm -rf $DEPLOY_PATH/*
 
 sudo cp $BUILD_WAR $DEPLOY_PATH
 
