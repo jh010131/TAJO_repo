6cf6bd9b65027e9fe3177ad27abac178a81e2170
jh010131 <loojinhwan@gmail.com>
2023-11-29 10:14:14 +0000

    modify web_start

--- scripts/web_start.sh
+++ scripts/web_start.sh
@@ -3,7 +3,7 @@
 cd /home/ec2-user
 
 # 웹 서버에 복사할 정적 파일들이 있는 디렉토리
-SOURCE_DIR="/home/ec2-user/build/static/"
+SOURCE_DIR="/home/ec2-user/build/static"
 
 # 웹 서버의 정적 파일이 위치할 디렉토리
 WEB_DIR="/var/www/html"
