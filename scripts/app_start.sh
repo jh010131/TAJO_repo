ab800aa9f047752c563d28260536bbeda3ea4cee
jh010131 <loojinhwan@gmail.com>
2023-11-29 08:26:20 +0000

    deploy update

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-BUILD_WAR="/home/ec2-user/build/spring-petclinic.war" 
+BUILD_WAR="/home/ec2-user/build/spring-petclinic.war"
 DEPLOY_PATH="/home/tomcat/apache-tomcat-10.1.12/webapps"
 TOMCAT_STOP="/home/tomcat/apache-tomcat-10.1.12/bin/shutdown.sh"
 TOMCAT_START="/home/tomcat/apache-tomcat-10.1.12/bin/startup.sh"
 
-sudo chmod +x $DEPLOY_PATH
+chmod +x $DEPLOY_PATH
 
-sudo rm -rf $DEPLOY_PATH/*
+rm -rf $DEPLOY_PATH/*
 
-sudo cp $BUILD_WAR $DEPLOY_PATH
+cp $BUILD_WAR $DEPLOY_PATH
 
-sudo $TOMCAT_STOP
-sudo $TOMCAT_START
+$TOMCAT_STOP
+$TOMCAT_START
