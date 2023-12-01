8162edefb3956f2fb7eacc1b53b520d2a78f667b
jh010131 <loojinhwan@gmail.com>
2023-12-01 13:25:35 +0000

    update backup scripts

---
+++ scripts/backup_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# 로컬 디렉토리 경로
+SOURCE_DIRECTORY="/home/ec2-user/environment/TAJO_repo"
+
+# S3 버킷 이름
+S3_BUCKET="tajo-source-backup"
+
+# S3 버킷 내 경로 (버전 관리를 위해 업로드 날짜 및 시간 사용)
+S3_PREFIX="$(date +"%Y-%m")/source-code-backup$(date +"%d")"
+
+# AWS CLI를 사용하여 로컬 디렉토리를 S3로 동기화
+sudo aws s3 sync "$SOURCE_DIRECTORY" "s3://$S3_BUCKET/$S3_PREFIX"
