cb19436c460b2abc9a0e4f29a30adc1469da1783
jh010131 <loojinhwan@gmail.com>
2023-12-07 05:09:54 +0000

    push svc

--- src/main/java/org/springframework/samples/petclinic/owner/SlackController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/SlackController.java
@@ -4,29 +4,32 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import slack.Slack;
-import slack.api.Message;
-import slack.api.SendMessage;
-
-import jakarta.validation.Valid;
+import com.slack.api.Slack;
+import com.slack.api.methods.request.chat.ChatPostMessageRequest;
+import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 
 @RestController
 @RequestMapping("/api")
 public class SlackController {
 
-    @Value("${slack.webhook-url}")
-    private String slackWebhookUrl;
+	@Value("${slack.token}")
+	private String slackToken;
 
-    @PostMapping("/send-slack-message")
-    public String sendSlackMessage() {
-        try {
-            Slack slack = Slack.getInstance();
-            SendMessage sendMessage = new SendMessage("#test", "Hello from Spring Boot!");
-            Message message = slack.call(sendMessage, slackWebhookUrl);
-            return "Slack message sent successfully. Message ID: " + message.getTimestamp();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return "Error sending Slack message.";
-        }
-    }
+	@PostMapping("/send-slack-message")
+	public String sendSlackMessage() {
+		try {
+			Slack slack = Slack.getInstance();
+			ChatPostMessageRequest request = ChatPostMessageRequest.builder()
+				.channel("#test")
+				.text("Hello from Spring Boot!")
+				.build();
+			ChatPostMessageResponse response = slack.methods(slackToken).chatPostMessage(request);
+			return "Slack message sent successfully. Message ID: " + response.getTs();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+			return "Error sending Slack message.";
+		}
+	}
+
 }
